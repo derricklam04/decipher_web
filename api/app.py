9e3219d9d06f478d341218683fad0b66e60f28c8
Derrick Lam <derricklam04@yahoo.com>
2021-01-11 18:20:36 -0500

    cors

--- api/app.py
+++ api/app.py
@@ -1,38 +1,20 @@
 from flask import Flask, jsonify, request, json
 # from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
+
 from algorithm import *
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.db"
-# db = SQLAlchemy(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-# class Card(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     content = db.Column(db.Text, nullable=False) # cannnot be empty
-#     key = db.Column(db.Text, nullable=True)
-#     keyLength = db.Column(db.Integer, nullable=True)
-#     translated = db.Column(db.Text, nullable=True)
-#     translateType = db.Column(db.Text, nullable=True)
-
-#     def __str__(self):
-#         return f'{self.id} {self.content} {self.key} {self.keyLength} {self.translated} {self.translateType}'
-
-def card_to_json(card):
-    return{
-        'id': card.id,
-        'content': card.content,
-        'key': card.key,
-        'keyLength': card.keyLength,
-        'translated': card.translated,
-        'translateType': card.translateType
-    }
 @app.route('/', methods =['GET'])
 def default():
-    return jsonify({'res':'hi'})
+    return jsonify({'start':'hello'})
 
-@app.route('/api', methods =['GET'])
-def index():
-    return jsonify([*map(card_to_json, Card.query.all())])
+# @app.route('/api', methods =['GET'])
+# def index():
+#     return jsonify([*map(card_to_json, Card.query.all())])
 
 @app.route('/api/create', methods = ['POST'])
 def create():
@@ -74,25 +56,6 @@ def create():
 
     return {'translatedText': translated, 'key':request_data['key'], 'error':'false', 'multiple': 'false'}
 
-@app.route('/api/<int:id>', methods = ['POST'])
-def delete(id):
-    request_data = json.loads(request.data)
-    Card.query.filter_by(id=request_data['id']).delete()
-    db.session.commit()
-
-    message = 'Card '+str(id)+' deleted successfully'
-    return {'204': message}
-
-@app.route('/api/add', methods = ['POST'])
-def add():
-    request_data = json.loads(request.data)
-    card = Card(content=request_data['content'],key=request_data['key'], translated=request_data['translated'], translateType=request_data['type'])
-
-    db.session.add(card)
-    db.session.commit()
-
-    message = 'Card '+str(card.id)+' created'
-    return {'201': message}
 
 if __name__ == '__main__':
     app.run(debug=True)
